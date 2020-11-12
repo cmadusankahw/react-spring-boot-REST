f24ba270e9e8cbbfa2ff76e8e22fa12b824f9134
Chiran Hewawitharana <chiranh@zone24x7.com>
2020-11-12 16:18:39 +0530

    initial commit

---
+++ backend/src/test/java/com/chiran/hewawitharana/react/spring/boot/rest/RESTApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiran.hewawitharana.react.spring.boot.rest.RESTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
